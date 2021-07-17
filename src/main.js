3e95d5265eb5350723c59a4d8e87aba7e72d272a
Inseo Hwang <sjsjsj1246@gmail.com>
2021-07-18 03:11:36 +0900

    ✨ static 배포

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import send from 'koa-send';
+import path from 'path';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
 
@@ -30,6 +33,17 @@ app.use(jwtMiddleware);
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(
+  __dirname,
+  '../../react_seminar_3week/build',
+);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
